05247938a8ba93b952b98e2cbec2f3395a52fa96
Matheus Ramyres <matheus.ramyres@oracle.com>
2024-02-26 15:09:40 -0300

    feat: my changes

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,25 @@ export const dataFake = [
   {
     "id":"1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "description": "Novo Lançamento do novo God of War chega as bancas",
+    "photoCover":"https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png?w=440"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Melhor Jogo de 2018",
+    "description": "God of war está entre os melhores jogos já lançados em 2018",
+    "photoCover":"https://image.api.playstation.com/vulcan/img/rnd/202011/1021/X3WIAh63yKhRRiMohLoJMeQu.png?w=440"
+  },
+  {
+    "id":"3",
+    "title": "Horizon Forbidden West",
+    "description": "Horizon Forbidden West um dos melhores do ano",
+    "photoCover":"https://image.api.playstation.com/vulcan/ap/rnd/202107/3100/ki0STHGAkIF06Q4AU8Ow4OkV.png?w=440"
+  },
+  {
+    "id":"4",
+    "title": "Horizon Zero Dawn",
+    "description": "Horizon Zero Dawn: Complete Edition primeiro jogo da franquia anida faz sucesso",
+    "photoCover":"https://image.api.playstation.com/vulcan/img/rnd/202011/1018/9erXiwu0ozgKGPIUVTuy2siN.png?w=440"
   }
 ]
